f91c9e3ecd1d41d46a591245caa1180522d863cf
Mostafa <mostafaasd2018@gmail.com>
2024-03-23 00:08:01 +0200

    Add Logging and Fix Scripts
    1- Add Console Logging To Selenium  Scrapping Script To track its progress easily
    2- Update the package.json scripts

--- dist/src/auth/selenium/scraper.js
+++ dist/src/auth/selenium/scraper.js
@@ -6,6 +6,7 @@ async function scrapeProfile(url) {
     if (!process.env.LINKEDIN_USERNAME || !process.env.LINKEDIN_PASSWORD) {
         throw new Error("Couldn't Scrap the linkedin profile, Please provide your LinkedIn username and password in the .env file");
     }
+    console.log('Starting Scraping LinkedIn Profile Session...');
     const host = 'localhost';
     let driver;
     let curTitle;
@@ -14,33 +15,49 @@ async function scrapeProfile(url) {
             .forBrowser('chrome')
             .usingServer(`http://${host}:4444/wd/hub`)
             .build();
+        console.log('Going To Login Page...');
         await driver.get('https://www.linkedin.com/login');
         await driver.manage().setTimeouts({ implicit: 5000 });
+        console.log('Done');
+        curTitle = await driver.getTitle();
+        console.log('Current Page Title:', curTitle);
+        console.log('Entering Credentials To Login...');
         const emailInput = await driver.findElement(selenium_webdriver_1.By.id('username'));
         const passwordInput = await driver.findElement(selenium_webdriver_1.By.id('password'));
         await emailInput.sendKeys(`${process.env.LINKEDIN_USERNAME}`);
         await passwordInput.sendKeys(`${process.env.LINKEDIN_PASSWORD}`, selenium_webdriver_1.Key.RETURN);
         await driver.manage().setTimeouts({ implicit: 5000 });
+        console.log('Done');
         curTitle = await driver.getTitle();
-        console.log('afterSignin: ', curTitle);
+        console.log('Current Page Title:', curTitle);
+        console.log('Going to profile Page...');
         await driver.get(url);
         await driver.manage().setTimeouts({ implicit: 5000 });
+        console.log('Done');
         curTitle = await driver.getTitle();
-        console.log('after going to profile: ', curTitle);
+        console.log('Current Page Title:', curTitle);
+        console.log('Scraping Profile Data...');
         const photoSection = await driver.findElement(selenium_webdriver_1.By.css('.pv-top-card--photo'));
+        const name = curTitle.split(' | ')[0];
         const avatar = await photoSection
             .findElement(selenium_webdriver_1.By.css('img'))
             .getAttribute('src');
-        const name = curTitle.split(' | ')[0];
-        console.log('name:', name);
-        console.log('avatar:', avatar);
+        console.log('Done');
+        console.log('Profile name:', name);
+        console.log('Profile Personal image link:', avatar);
+        console.log('Ending Scraping LinkedIn Profile Session Successfully...');
         return { name, avatar };
     }
     catch (e) {
-        console.error(e);
+        console.log('End Scraping LinkedIn Profile Session With Error...');
+        console.error('error in scraping linkedin profile:', e);
+        console.log('Make sure that:');
+        console.log('1- The linkedin profile is valid And public');
+        console.log("2- The linkedin Account doesn't require a sceurity verification after login");
+        console.log('3- The Remote Docker Selenium Server is running on the host machine correctly');
     }
     finally {
-        await driver.quit();
+        await driver?.quit();
     }
 }
 exports.scrapeProfile = scrapeProfile;
--- src/auth/selenium/scraper.ts
+++ src/auth/selenium/scraper.ts
@@ -12,6 +12,8 @@ export async function scrapeProfile(url: string): Promise<ProfileData> {
     );
   }
 
+  console.log('Starting Scraping LinkedIn Profile Session...');
+
   // const host = 'selenium-hub';
   const host = 'localhost';
   let driver: WebDriver;
@@ -23,40 +25,62 @@ export async function scrapeProfile(url: string): Promise<ProfileData> {
       .build();
 
     // Login to LinkedIn
+    console.log('Going To Login Page...');
     await driver.get('https://www.linkedin.com/login');
     await driver.manage().setTimeouts({ implicit: 5000 });
+    console.log('Done');
+
+    curTitle = await driver.getTitle();
+    console.log('Current Page Title:', curTitle);
+
+    console.log('Entering Credentials To Login...');
     const emailInput = await driver.findElement(By.id('username'));
     const passwordInput = await driver.findElement(By.id('password'));
-
-    await emailInput.sendKeys(`${process.env.LINKEDIN_USERNAME}`); // Add your LinkedIn email Address
+    await emailInput.sendKeys(`${process.env.LINKEDIN_USERNAME}`);
     await passwordInput.sendKeys(
       `${process.env.LINKEDIN_PASSWORD}`,
       Key.RETURN,
-    ); // Add your LinkedIn Password
+    );
     await driver.manage().setTimeouts({ implicit: 5000 });
+    console.log('Done');
 
     curTitle = await driver.getTitle();
-    console.log('afterSignin: ', curTitle);
+    console.log('Current Page Title:', curTitle);
 
+    console.log('Going to profile Page...');
     await driver.get(url);
-
     await driver.manage().setTimeouts({ implicit: 5000 });
+    console.log('Done');
+    
     curTitle = await driver.getTitle();
-    console.log('after going to profile: ', curTitle);
+    console.log('Current Page Title:', curTitle);
+
+    console.log('Scraping Profile Data...');
     const photoSection = await driver.findElement(
       By.css('.pv-top-card--photo'),
     );
+    const name = curTitle.split(' | ')[0];
     const avatar = await photoSection
       .findElement(By.css('img'))
       .getAttribute('src');
+    console.log('Done');
 
-    const name = curTitle.split(' | ')[0];
-    console.log('name:', name);
-    console.log('avatar:', avatar);
+    console.log('Profile name:', name);
+    console.log('Profile Personal image link:', avatar);
+    console.log('Ending Scraping LinkedIn Profile Session Successfully...');
     return { name, avatar };
   } catch (e) {
-    console.error(e);
+    console.log('End Scraping LinkedIn Profile Session With Error...');
+    console.error('error in scraping linkedin profile:', e);
+    console.log('Make sure that:');
+    console.log('1- The linkedin profile is valid And public');
+    console.log(
+      "2- The linkedin Account doesn't require a sceurity verification after login",
+    );
+    console.log(
+      '3- The Remote Docker Selenium Server is running on the host machine correctly',
+    );
   } finally {
-    await driver.quit();
+    await driver?.quit();
   }
 }
